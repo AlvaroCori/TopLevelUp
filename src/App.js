6a4663e66d2412348c8dc41c622a59601cd4a148
AlvaroCori <bacs200018@gmail.com>
2022-03-02 16:48:04 -0400

    ready

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+function title(mainImageUrl)
+{
+  return (
+    <div>
+        <img src={mainImageUrl}></img>
+        
+          <p>La siguiente app comienza aca.</p>
+        </div>
+  );
+}
 function App() {
   return (
     <div className="App">
@@ -18,6 +27,10 @@ function App() {
           Learn React
         </a>
       </header>
+      <body>
+        <h1>PRIMERA APP DE PRUEBA</h1>
+        <div>{title("https://i.redd.it/olqp7fj5svs51.jpg")}</div>   
+      </body>
     </div>
   );
 }
